b653fc8e9818873f4ed0efdc9815cdb6d2e62b38
issdn <karol.bielski@gmx.de>
2025-04-14 21:38:11 +0200

    WIP landing page

---
+++ src/lib/svg/DOCX.svelte
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22.96 22">
+  <defs>
+    <style>
+      .cls-1 {
+        fill: #fff;
+        font-family: Arial-BoldMT, Arial;
+        font-size: 6px;
+        font-weight: 700;
+      }
+
+      .cls-2 {
+        fill: none;
+        stroke: #000;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke-width: 2px;
+      }
+    </style>
+  </defs>
+  <path class="cls-2" d="M14.45,1H5.45c-1.1,0-2,.9-2,2v16c0,1.1.9,2,2,2h12c1.1,0,2-.9,2-2V6L14.45,1Z"/>
+  <path class="cls-2" d="M13.45,1v4c0,1.1.9,2,2,2h4"/>
+  <rect y="11.45" width="22.96" height="6.91" rx="1.42" ry="1.42"/>
+  <text class="cls-1" transform="translate(2.63 17.04)"><tspan x="0" y="0">DOCX</tspan></text>
+</svg>
