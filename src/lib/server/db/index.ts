849083c656d2a7ee7c676a2d86ac6066098b951f
issdn <karol.bielski@gmx.de>
2025-05-20 19:30:08 +0200

    remove unused deps

--- src/lib/server/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '$env/dynamic/private';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { dev } from '$app/environment';
-import postgres from 'postgres';
-
-const client = postgres(dev ? env.LOCAL_POSTGRES_URL! : env.POSTGRES_URL!, { prepare: false });
-export const db = drizzle({ client });
