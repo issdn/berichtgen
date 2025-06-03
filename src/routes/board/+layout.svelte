51712e95dcdc9770975fedb4331ae59bd8ac734c
issdn <karol.bielski@gmx.de>
2025-06-03 23:53:03 +0200

    in dev notification

--- src/routes/board/+layout.svelte
+++ src/routes/board/+layout.svelte
@@ -2,6 +2,7 @@
 	import { berichtgenStore } from '$src/lib/stores/berichtgen.svelte.js';
 	import { type UserContext } from '$src/lib/types.js';
 	import { getContext, onMount } from 'svelte';
+	import { toast } from 'svelte-sonner';
 
 	let { data, children } = $props();
 
@@ -10,6 +11,10 @@
 	berichtgenStore.providers = data.providers;
 
 	onMount(() => {
+		toast.info(
+			'Diese App is aktuell in der Entwicklung und kann Fehler enthalten. Anmeldung ist deaktiviert!',
+			{ dismissable: true, duration: 10000 }
+		);
 		berichtgenStore.processPhotos = JSON.parse(localStorage.getItem('processPhotos') ?? 'false');
 		berichtgenStore.rewordJSON = JSON.parse(localStorage.getItem('rewordJSON') ?? 'false');
 		if (loggedIn && data.providers.length > 0) {
