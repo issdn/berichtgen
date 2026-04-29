5e1b02869f55fc5da3f18d7588913ed6aeb65afd
Karol Bielski <karol.bielski@gmx.de>
2026-04-29 22:17:57 +0200

    ssl

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,13 +1,18 @@
+import { dev } from '$app/environment';
 import { DATABASE_URL } from '$env/static/private';
 import type { KyselyDatabase } from '$lib/schema';
 import { Kysely, PostgresDialect } from 'kysely';
 import pg from 'pg';
 
+const ssl = dev
+	? undefined
+	: { rejectUnauthorized: true, ca: process.env.SUPABASE_CA };
+
 const db = new Kysely<KyselyDatabase>({
 	dialect: new PostgresDialect({
 		pool: new pg.Pool({
 			connectionString: DATABASE_URL,
-			ssl: { rejectUnauthorized: true, ca: process.env.SUPABASE_CA }
+			ssl
 		})
 	})
 });
